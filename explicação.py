f416f490fce2a2fb65dd8e82b8bf82dcc7df2b10
gueldev <169162437+gueldev@users.noreply.github.com>
2024-06-07 07:45:45 -0300

    Add files via upload

---
+++ explicação.py
@@ -0,0 +1,15 @@
+#FUNÇÕES:
+
+# procurar_carro(nome_carro, lista_de_carros): Essa função recebe o nome do carro desejado e a lista de carros disponíveis. Ela percorre a lista e verifica se o carro desejado está presente nela. Retorna "carro encontrado" se o carro estiver na lista, caso contrário, retorna "carro não encontrado".
+# avaliacao_carro(preco_carro): Essa função recebe o preço do carro desejado e avalia sua condição com base nesse preço. Retorna uma string indicando a condição do carro: "Mal estado", "Conservado", "Seminovo" ou "Novo".
+# main(): Essa função inicializa a lista de carros disponíveis e entra em um loop infinito onde solicita ao usuário o nome e o preço do carro desejado. Depois, imprime uma mensagem de confirmação com os dados fornecidos, e chama as funções procurar_carro() e avaliacao_carro() para verificar se o carro está disponível e avaliar sua condição, no fim oferece a opção de continuar ou encerrar o programa.
+
+#EXECUÇÃO DO PROGRAMA:
+
+# o programa começa chamando a função 'main'
+# dentro dessa função, o usuário é solicitado o nome do carro desejado e o preço no qual gostaria de pagar
+# o programa chama as funções procurar_carro() e avaliacao_carro() para ver se tem o carro na concessionária e avaliar sua condição.
+# se o carro estiver disponível, é exibida uma mensagem de confirmação juntamente com a qualidade de conservação do carro.
+# o usuário é solicitado a decidir se deseja continuar ou encerrar o programa, caso ele opte por não prosseguir pesquisando, o programa se encerra
+
+
